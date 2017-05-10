ebb651e76588206bf2ac619f59506c8b92fa7ca6
BogdanBetsa <bogdanbeca1991@gmail.com>
2017-05-10 20:53:04 +0300

    third commit

--- main.js
+++ main.js
@@ -1,3 +1,89 @@
 console.log("hello");
 var userName = "hello\" world";
 console.log(userName);
+var x=5;
+console.log(x);
+var x=3e5;
+console.log(x);
+var x=3e-5;
+console.log(x);
+var x=0xFF;
+console.log(x);
+var x=0b110001;
+console.log(x);
+var x=0123;
+console.log(x);
+var x=5;
+console.log(typeof x)
+var x="hello";
+console.log(typeof x)
+var x=null;
+console.log(typeof x)
+var x;
+console.log(typeof x)
+var x=5;
+var y=6;
+console.log(x+y);
+var x=5;
+var y=6;
+x=7;
+console.log(x+y);
+var x=5;
+console.log("hello"+x);
+var x=5,
+    y=7,
+    c=x*y;
+console.log(c);
+var x=5,
+    y=7,
+    c=x+y*x;
+console.log(c);
+var x=5,
+    y=7,
+    c=(x+y)*x;
+console.log(c);
+var name="vasya",
+    say="hello ";
+console.log(say+name);
+var name="vasya",
+    say="hello";
+console.log(say+" "+name);
+var name="vasya",
+    say="hello";
+console.log(say*5);
+var name="vasya",
+    say="385";
+console.log(say*5);
+var name="vasya",
+    say=+"385";
+console.log(typeof say);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
