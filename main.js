52b0d0b1d796a5609cb5e426b307f8fa81d57141
BogdanBetsa <bogdanbeca1991@gmail.com>
2017-05-15 21:00:49 +0300

    story seven

--- main.js
+++ main.js
@@ -162,3 +162,120 @@ switch (month) {
         console.log("Вы ввели неверно месяц");
         break;
 }*/
+/*var arr = [23, null, 'vasya', true, [32, 3]];
+console.log(arr[4][0]);
+
+var arr = [23, null, 'vasya', true, [32, 3]];
+arr[1] = 'petya'
+console.log(arr);
+
+var arr = [23, null, 'vasya', true, [32, 3]];
+arr[1] = 'petya'
+console.log(arr.length);
+
+var str = "hello world"
+console.log(str[0]);
+
+var arr = [23, null, 'vasya', true, [32, 3]];
+arr[1] = 'petya'
+arr[10] = 13;
+console.log(arr);
+
+var user = {
+    name: "Misha",
+    age: 35,
+    gender: "Male",
+    email: "misha@gmail.com"
+};
+user.age
+user["name"]
+
+var users = [
+    {
+        name: "Misha",
+        age: 35,
+        email: "misha@gmail.com"
+    },
+    {
+        name: "Sasha",
+        age: 38,
+        email: "sasha@gmail.com"
+    },
+    {
+        name: "Kolya",
+        age: 40,
+        email: "kolya@gmail.com"
+    }
+];
+users[1].name
+
+var smartphone = {
+    size: {
+        value: 5,
+        metric: "inch"
+    },
+    color: "black",
+    brand: "meizu",
+    model: "MX3",
+    phonebook: [
+        {
+            name: "Misha",
+            tel: "30507654321"
+        },
+        {
+            name: "Gregori",
+            tel: "380501234567"
+        }
+    ]
+};
+console.log(smartphone.brand)
+console.log(smartphone.phonebook[0].tel)*/
+
+/*var i;
+for (i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+var users = ["Kolya", "Misha", "Masha", "Grisha"];
+for (var i = 0; i < users.length; i++) {
+    console.log(users[i]);
+    if (users[i] == "Misha") {
+        break;
+    }
+}
+
+var i = 0;
+while (i < 10) {
+    i++;
+}
+
+/*var q = true;
+do {
+    q = confirm("Вы действительно хотите удалить?");
+} while (q);*/
+
+var a = {name: "Petya", age:40, gender:"Male"};
+for(var u in a){
+    console.log(a[u])
+};
+
+var a = [34, 5, 40, 60];
+for(var u in a){
+    console.log(a[u])
+};
+
+var menu = [
+    {
+        title: "",
+        link: "",
+        submenu:[
+            {   }
+            {   }
+        ]
+    }
+];
+ 
+
+
+
+
